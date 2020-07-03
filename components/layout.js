a358e160c9129b67dffc98fa37cb655e588e54ba
Mike Cao <mike@mikecao.com>
2020-07-02 18:24:56 -0700

    Moved syntax css file.

--- components/layout.js
+++ components/layout.js
@@ -7,14 +7,12 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Flight - An extensible micro-framework for PHP</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Mr+Dafoe&family=Open+Sans&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="stylesheet" type="text/css" href="/syntax.css" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-101933-10"></script>
         <script
           dangerouslySetInnerHTML={{
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'styles/index.css';
+import 'styles/syntax.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
