8686a575d4f6e2d3236258ae0cedab1da07be363
Mike Cao <mike@mikecao.com>
2023-05-12 19:50:05 -0700

    Added mixpanel.

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,26 @@ import React from 'react';
 import 'styles/bootstrap-grid.css';
 import 'styles/index.css';
 import 'styles/syntax.css';
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('ecb2430cbf1503f9566b58e4eb9a3294', { debug: true });
+
+function getRandomChars(
+  n,
+  chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+) {
+  const arr = chars.split('');
+  let s = '';
+  for (let i = 0; i < n; i++) {
+    s += arr[Math.floor(Math.random() * arr.length)];
+  }
+  return s;
+}
 
 export default function App({ Component, pageProps }) {
+  mixpanel.track('visit', {
+    random: getRandomChars(16),
+  });
+
   return <Component {...pageProps} />;
 }
